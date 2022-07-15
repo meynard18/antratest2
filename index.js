b2cf3cd1922f93e2039b19b10835aea422173b0c
Meynard Duran <pvt_mduran18@yahoo.com>
2022-07-15 14:39:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const url = 'http://localhost:4232/movies';
+
+const btnRight = document.querySelector('.btn-right');
+const btnLeft = document.querySelector('.btn-left');
+
+const fetchData = async () => {
+   const response = await fetch(url);
+   const data = await response.json();
+   displayData(data);
+};
+
+const displayData = (movies) => {
+   const cardContainerEl = document.querySelector('.cardContainer');
+   const cards = document.querySelector('.cardContainer');
+   const movieString = movies
+      .map((item) => {
+         return `
+            <div class="cards">
+                <img class="image" src="${item.imgUrl}"/>
+                <h3 class="movieName">Movie: ${item.name}</h3>
+                <h3 class="movieInfo">Info: ${item.outlineInfo}</h3>
+            </div>
+      `;
+      })
+      .join('');
+   cards.innerHTML = movieString;
+
+   const slides = Array.from(cardContainerEl.children);
+   const slideWidth = slides[0].getBoundingClientRect().width;
+
+
+   ///////  SCROLL RIGHT //////////
+   btnRight.addEventListener('click', () => {
+      cardContainerEl.scrollLeft += slideWidth * 4;
+      btnLeft.classList.add('displayLeft');
+   });
+
+   /////// SCROLL LEFT ///////////////
+   btnLeft.addEventListener('click', () => {
+      cardContainerEl.scrollLeft -= slideWidth * 4;
+      if (cardContainerEl.scrollLeft < 1040) {
+         btnLeft.classList.remove('displayLeft');
+      }
+   });
+};
+
+fetchData();
